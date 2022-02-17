18a215a6ee8ffa0033826a457c7d211fc7d487ef
PawelMurdza <pawel.murdza@gmail.com>
2022-02-16 19:53:36 -0500

    1stst

---
+++ src/gitjob/abc.java
@@ -0,0 +1,11 @@
+package gitjob;
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("water is colorless");
+System.out.println("not bluegfdg");
+	}
+
+}
